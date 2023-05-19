7c8eb535def2c3801392caf1a7ecb87de1abcc42
Jared Aragon <jaredaragon7@gmail.com>
2023-05-18 22:38:38 -0600

    adds file structure

---
+++ models/User.js
@@ -0,0 +1,45 @@
+const { Schema, model } = require('mongoose');
+
+// Schema to create a user model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true
+    },
+    email: {
+      type: Boolean,
+      required: true,
+      unique: true,
+      match: [/\w+@\w+.\w{2,3}+/, "Invalid email"]
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought',
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length
+})
+
+const User = model('user', userSchema);
+
+module.exports = User;
